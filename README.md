840087e3d242ad476d5ee23f05082156354639cc
Christal D <179974249+ccdita@users.noreply.github.com>
2025-06-06 09:52:26 -0400

    Add eventListeners to grid, update README.md
    This commit adds a "hover" effect to each square in the grid, so that
    target squares change color upon being hovered on. This allows the user
    to "sketch" on the grid. Additionally, this commit adds challenges and
    lessons gained so far during the creation of this project.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# etch-a-sketch
+Challenges:
+Making a grid of squares
+- Had to review flexbox after 2 weeks
+- First attempted to make a continuous line of squares and then have them wrap to new lines to form a proper grid using flex and properties such as height and width
+- First attempt did not work so instead implemented for loops that made divs (rows and columns/squares)
+
+Learned:
+- Why script tag is put at end of HTML body (DOM loading)
+- flex: 1's and aspect-ratio: 1/1's effect on width and height of children
--- script.js
+++ script.js
@@ -12,6 +12,13 @@ for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
         divColumn = document.createElement("div");
         divColumn.classList.add("divColumn");
+        // Add event listener
+        divColumn.addEventListener("mouseenter", (e) => {
+            // Change the event target's background color
+            // Don't use divColumn as this would only change the last divColumn to pink since divColumn
+            // is reused with each loop
+            e.target.style.backgroundColor = "pink";
+        })
         divRow.appendChild(divColumn);
     }
     divContainer.appendChild(divRow);
