85af18256d6598c3490c55a370496cbbc26140df
Christal D <179974249+ccdita@users.noreply.github.com>
2025-06-06 11:50:02 -0400

    Add button that prompts user for new canvas size
    This commit adds a button above the grid that prompts the user for a new
    canvas size upon being clicked. This button will allow the user to
    create a new canvas whose size will depend on their input.

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const n = 16;
+let n = 16;
 // Select div that will contain the grid
 const divContainer = document.querySelector("#container");
 
-let divRow;
-let divColumn;
-// Make a n * n grid of squares
-for (i = 0; i < n; i++) {
-    // 16 rows, each of which will contain 16 divs (squares)
-    divRow = document.createElement("div");
-    divRow.classList.add("divRow");
-    for (j = 0; j < n; j++) {
-        divColumn = document.createElement("div");
-        divColumn.classList.add("divColumn");
-        // Add event listener
-        divColumn.addEventListener("mouseenter", (e) => {
-            // Change the event target's background color
-            // Don't use divColumn as this would only change the last divColumn to pink since divColumn
-            // is reused with each loop
-            e.target.style.backgroundColor = "pink";
-        })
-        divRow.appendChild(divColumn);
+const newCanvasBtn = document.querySelector("#new-canvas");
+newCanvasBtn.addEventListener("click", () => {
+    let input = prompt("Enter size of new canvas:");
+    console.log(input);
+});
+
+function createGrid(n) {
+    let divRow;
+    let divColumn;
+    // Make a n * n grid of squares
+    for (i = 0; i < n; i++) {
+        // 16 rows, each of which will contain 16 divs (squares)
+        divRow = document.createElement("div");
+        divRow.classList.add("div-row");
+        for (j = 0; j < n; j++) {
+            divColumn = document.createElement("div");
+            divColumn.classList.add("div-column");
+            // Add event listener
+            divColumn.addEventListener("mouseenter", (e) => {
+                // Change the event target's background color
+                // Don't use divColumn as this would only change the last divColumn to pink since divColumn
+                // is reused with each loop
+                e.target.style.backgroundColor = "black";
+            })
+            divRow.appendChild(divColumn);
+        }
+        divContainer.appendChild(divRow);
     }
-    divContainer.appendChild(divRow);
 }
+
+createGrid(n);
